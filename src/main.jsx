91e82e13e4353db5f3d02c604a2088511f843fa1
Venu Bhargav Jishith Kasibhatla <venu.kasibhatla@gmail.com>
2026-02-21 21:22:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import "./index.css";
 import App from "./App.jsx";
-import { Analytics } from "@vercel/analytics/react"
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
