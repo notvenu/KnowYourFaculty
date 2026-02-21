f7e8545f449b6483bd0546536188f131047ab1dd
Venu Bhargav Jishith Kasibhatla <venu.kasibhatla@gmail.com>
2026-02-21 21:01:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </Provider>
   </StrictMode>,
